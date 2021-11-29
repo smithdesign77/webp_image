e6249b019c17dded997d8ba71ae5a7f3c5af50bc
Yuri <wdwp@bk.ru>
2021-11-29 12:27:19 +0300

    Add files via upload
    Some improvements

--- function.webp_image.php
+++ function.webp_image.php
@@ -24,28 +24,33 @@
 
 function smarty_function_webp_image($params, &$smarty)
 {
+  $gCms = CmsApp::get_instance();
 
-	#Get parameters from the function call
-  $src = isset($params['src']) ? $params['src'] : '';
+  $root_path = $gCms->config['root_path'];
+
+  #Get parameters from the function call
+  $src = isset($params['src']) ? str_replace(array($gCms->config['root_url'] . '/', '//'), array('', '/'), $params['src']) : '';
+
+  $original_src = isset($params['src']) ? $params['src'] : '';
 
   $quality = isset($params['quality']) ? $params['quality'] : 90;
 
   #Get source path
-  $path = parse_url($src, PHP_URL_PATH);
+  $path = $root_path . '/' . urldecode($src);
 
-  if(file_exists($path)) {
+  if (file_exists($path)) {
 
     #Get source info
     $pathinfo = pathinfo($path);
 
     $extension = $pathinfo['extension'];
 
-    $destination = 'tmp/cache/img-'.md5_file($path).'.webp';
+    $destination = $root_path . '/tmp/cache/img-' . md5_file($path) . '.webp';
 
-    # If file alredy exists, return it
+    # If file already exists, return it
     if (file_exists($destination) && strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false)
 
-      return $destination;
+      return str_replace($root_path . '/', '', $destination);
 
     if (strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false) {
 
@@ -59,10 +64,7 @@ function smarty_function_webp_image($params, &$smarty)
         imagepalettetotruecolor($image);
         imagealphablending($image, true);
         imagesavealpha($image, true);
-
-      }
-
-      else return "Error: the image extension is wrong";
+      } else return "Error: the image extension is wrong";
 
       #webp image creation
       if ($image !== false) imagewebp($image, $destination, $quality);
@@ -70,42 +72,47 @@ function smarty_function_webp_image($params, &$smarty)
       else return "Error: the image has not been created";
 
       # If the browser supports webp. Hello Safari!
-      if (file_exists($destination) && strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false)
+      if (file_exists($destination) && strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false) {
 
         #Return encoded image
-        return $destination;
+        return str_replace($root_path . '/', '', $destination);
+      } else {
 
-      else  return $src;
-
-    } else return $src;
-
+        return $original_src;
+      }
+    } else return $original_src;
   } else {
 
     return 'Error: the image path is not valid';
-
   } //end if
 
-}// End Function
+} // End Function
 /**
  * Help text
  */
-function smarty_cms_help_function_webp_image() {
-  ?>
+function smarty_cms_help_function_webp_image()
+{
+?>
   <h3>What does this do?</h3>
   <p>This plugin converts an image to webp format.</p>
   <h3>How do I use it?</h3>
-  <p>HTML: &lt;img src="{webp_image src="uploads/simplex/images/cmsmadesimple-logo.png" quality="85"}" width="227" height="59"/&gt;
-  <?php
+  <p>HTML: &lt;img src="{webp_image src="uploads/simplex/images/cmsmadesimple-logo.png" quality="85"}" width="227" height="59"/&gt;</p>
+  <p>You can use an absolute or relative url to the image. For Gallery: </p>
+  <p>&lt;a class="group" href="{webp_image src=$image->file}" title="{$image->comment}" rel="prettyPhoto[{$galleryid}]"&gt;&lt;img src="{webp_image src=$image->thumb}" alt="{$image->titlename}" /&gt;&lt;/a&gt;</p>
+
+<?php
 } // End Function
 /**
  * About text
  */
-function smarty_cms_about_function_webp_image() {
-  ?>
+function smarty_cms_about_function_webp_image()
+{
+?>
   <p><b>Plugin author: Yuri Haperski (wdwp@yandex.ru)</b></p>
   <p><b>Version:</b> 1.0</p>
   <p><b>Change History:</b></p>
   <p><b>20-03-2021 - Initial release (v1.0)</b></p>
-  <?php
+  <p><b>20-11-2021 - Some improvements (v1.1)</b></p>
+<?php
 } // End Function
 ?>
